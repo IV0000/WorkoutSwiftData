44566abcf9921314ef43bedf141096565f6e38c9
Ivan Voloshchuk <keranamv@gmail.com>
2023-06-08 19:04:23 +0200

    Add insert function

--- WorkoutSwiftData/ContentView.swift
+++ WorkoutSwiftData/ContentView.swift
@@ -10,18 +10,42 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var context
-    var workouts: [Workout] = []
+    @Query(sort: \.createdAt,order: .reverse) var allWorkouts: [Workout]
+    @State var workoutName: String = ""
+    @State var workoutDescription: String = ""
     var body: some View {
         VStack {
-            List(workouts) { workout in
-                Text(workout.name)
-                Text(workout.description)
+            List {
+                if allWorkouts.isEmpty {
+                    Section("New note"){
+                        TextField("Note name",text: $workoutName)
+                        TextField("Note description",text: $workoutDescription)
+                            .lineLimit(1...3)
+                        Button("Save") {
+                            createWorkout()
+                        }
+                    }
+                    ContentUnavailableView("You don't have workouts yet",systemImage: "gymdumbbell.fill")
+                } else {
+                    ForEach(allWorkouts) { workout in
+                        Text(workout.name)
+                        Text(workout.description)
+                    }
+                }
             }
         }
-        .padding()
+    }
+    func createWorkout() {
+        let workout = Workout(id: UUID().uuidString,
+                              name: workoutName,
+                              createdAt: Date(),
+                              description: workoutDescription,
+                              exerciseList: [])
+        context.insert(workout)
+        try? context.save()
     }
 }
 
 #Preview {
-    ContentView(workouts: [])
+    ContentView()
 }
--- WorkoutSwiftData/Model/Workout.swift
+++ WorkoutSwiftData/Model/Workout.swift
@@ -13,11 +13,13 @@ final class Workout {
     @Attribute(.unique) var id: String
     var name: String
     var description: String
+    var createdAt: Date
     var exerciseList: [Exercise] = [] //Add @Relationship
     
-    init(id: String, name: String, description: String, exerciseList: [Exercise]) {
+    init(id: String, name: String, createdAt: Date, description: String, exerciseList: [Exercise]) {
         self.id = id
         self.name = name
+        self.createdAt = createdAt
         self.description = description
         self.exerciseList = exerciseList
     }
@@ -25,9 +27,9 @@ final class Workout {
 
 extension Workout {
     static func mock() -> [Workout] {
-        [ Workout(id: "1", name: "Full Body Workout", description: "A comprehensive workout that targets all major muscle groups.", exerciseList: Exercise.mock()),
-          Workout(id: "2", name: "Cardio Blast", description: "An intense cardio workout to improve cardiovascular endurance.", exerciseList: Exercise.mock() ),
-          Workout(id: "3", name: "Leg Day", description: "Focuses on exercises for the lower body, including legs and glutes.", exerciseList: Exercise.mock() ),
-          Workout(id: "4", name: "Upper Body Strength", description: "Strength training exercises for the upper body, including arms, chest, and back.",exerciseList: Exercise.mock())]
+        [ Workout(id: "1", name: "Full Body Workout", createdAt: Date.now, description: "A comprehensive workout that targets all major muscle groups.", exerciseList: Exercise.mock()),
+          Workout(id: "2", name: "Cardio Blast", createdAt: Date.now+1, description: "An intense cardio workout to improve cardiovascular endurance.", exerciseList: Exercise.mock() ),
+          Workout(id: "3", name: "Leg Day", createdAt: Date.now+2, description: "Focuses on exercises for the lower body, including legs and glutes.", exerciseList: Exercise.mock() ),
+          Workout(id: "4", name: "Upper Body Strength", createdAt: Date.now+3, description: "Strength training exercises for the upper body, including arms, chest, and back.",exerciseList: Exercise.mock())]
     }
 }
